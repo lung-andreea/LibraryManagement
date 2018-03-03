43f4cf7f1f2b23a11514cf4411850ad1f26cd7d1
unknown <lung_andreea_cristina@yahoo.com>
2018-03-03 17:42:44 +0200

    lallaaaa

---
+++ src/Domain/Book.java
@@ -0,0 +1,51 @@
+package Domain;
+
+public class Book {
+    private String title;
+    private String author;
+    private Integer price;
+    private Integer stock;
+
+    public Book(String title, String author, Integer price, Integer stock) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public String getAuthor() {
+
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+}
---
+++ src/Domain/Client.java
@@ -0,0 +1,27 @@
+package Domain;
+
+public class Client {
+    private String name;
+    private Integer amountSpent = 0;
+
+    public Client(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAmountSpent() {
+        return amountSpent;
+    }
+
+    public void setAmountSpent(Integer amountSpent) {
+        this.amountSpent = amountSpent;
+    }
+}
