97bdf8bab9a05c7e063e4b5cb884f4dee7e76974
unknown <lung_andreea_cristina@yahoo.com>
2018-03-12 22:44:50 +0200

    XML

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ import Domain.Validators.Validator;
 import Repository.FileRepository;
 import Repository.IRepository;
 import Repository.InMemoryRepository;
+import Repository.XMLRepository;
 import Tests.AllTests;
 import UI.Console;
 
@@ -24,39 +25,22 @@ public class Main {
 //        console.runConsole();
 
         // FileRepo (CSV)
-        IRepository clientsRepo = new FileRepository<Client>(new ClientValidator(),"clients.csv");
-        IRepository booksRepo = new FileRepository<Book>(new BookValidator(),"books.csv");
+//        IRepository clientsRepo = new FileRepository<Client>(new ClientValidator(),"clients.csv");
+//        IRepository booksRepo = new FileRepository<Book>(new BookValidator(),"books.csv");
+//        IRepository purchaseRepo=new InMemoryRepository<Integer,Purchase>(new PurchaseValidator());
+//        Console console = new Console(new BookController(booksRepo,clientsRepo,purchaseRepo));
+//        console.runConsole();
+
+        // XMLRepo
+        IRepository clientsRepo = new XMLRepository<Client>(new ClientValidator(),"clients.xml");
+        IRepository booksRepo = new XMLRepository<Book>(new BookValidator(),"books.xml");
         IRepository purchaseRepo=new InMemoryRepository<Integer,Purchase>(new PurchaseValidator());
         Console console = new Console(new BookController(booksRepo,clientsRepo,purchaseRepo));
         console.runConsole();
+}
 
-////        IRepository<Integer, Book> repo=new InMemoryRepository<>(new BookValidator());
-////
-////        Book b1=new Book("a","b",10,12);
-////        b1.setId(1);
-////        Book b2=new Book("b","c",12,12);
-////        b2.setId(2);
-////        try {
-////            repo.save(b1);
-////            repo.save(b2);
-////            for(Book b:repo.findAll())
-////                System.out.println(b);
-////            Book b3=new Book("c","c",-1,10);
-////            b3.setId(2);
-////            repo.update(b3);
-////            for(Book b:repo.findAll())
-////                System.out.println(b);
-////        }catch(ValidatorException e){System.out.println(e);}
-//
-//        IRepository<Integer, Client> repo=new InMemoryRepository<>(new ClientValidator());
-//
-//        Client c1=new Client("Roman");
-//        c1.setId(1);
-//        Client c2=new Client("Roman2");
-//        c2.setId(2);
-//        try {
-//            repo.save(c1);
-//            repo.save(c2);
-//        }catch (ValidatorException e){System.out.println(e);}
-//    }
-}}
+    private static void writeClientsToXML()
+    {
+
+    }
+}
